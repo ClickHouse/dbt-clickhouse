60e4ca078d112b72d2b62423e99ac37ab627505f
dbevz <dbevz@ivi.ru>
2021-04-25 20:26:57 +0300

    Added sqlalchemy to requirements

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
     install_requires=[
         f'dbt-core=={dbt_version}',
         'clickhouse-driver>=0.1.4',
+        'clickhouse_sqlalchemy==0.1.6'
     ],
     python_requires=">=3.6",
     platforms='any',
