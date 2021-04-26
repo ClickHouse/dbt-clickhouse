3492b079486321c95f964ae78ec8487ddea1d09e
dbevz <dbevz@ivi.ru>
2021-04-26 15:23:14 +0300

    One more time

--- dbt/include/clickhouse/macros/materializations/seed.sql
+++ dbt/include/clickhouse/macros/materializations/seed.sql
@@ -21,7 +21,7 @@
           {{ adapter.quote_seed_column(column_name, quote_seed_column) }} {{ type }} {%- if not loop.last -%}, {%- endif -%}
       {%- endfor -%}
     )
-    {{ engine_clause(label='engine', target=this.render()) }}
+    {{ engine_clause(label='engine', target=this.name) }}
     {{ order_cols(label='order by') }}
     {{ partition_cols(label='partition by') }}
   {% endset %}
