f93656c57aa461ced4f9362829f9a7d6612f720a
Dmitry Volodin <volodin.dd@outlook.com>
2021-11-17 21:44:47 +0300

    Fixed view (simplier and on cluster)

---
+++ dbt/include/clickhouse/macros/materializations/view.sql
@@ -0,0 +1,28 @@
+{%- materialization view, default -%}
+
+  {% set target_relation = this.incorporate(type='view') %}
+
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+
+  {{ drop_relation_if_exists(target_relation) }}
+
+
+  -- build model
+  {% do run_query(create_view_as(target_relation, sql)) %}
+
+  -- cleanup
+  -- move the existing view out of the way
+  
+
+  {% do persist_docs(target_relation, model) %}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  {{ drop_relation_if_exists(backup_relation) }}
+
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+  {{ return({'relations': [target_relation]}) }}
+
+{%- endmaterialization -%}
