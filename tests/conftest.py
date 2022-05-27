f3ac1b04d8b4217920c71cb786a287ca4d4c4f50
guykohen <guy@clickhouse.com>
2022-05-27 12:14:39 -0400

    switch clcikhouse driver to clickhouse-connect

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 from subprocess import Popen, PIPE
-
 import pytest
 from time import sleep
 import os
@@ -11,6 +10,7 @@ import timeit
 # Import the standard integration fixtures as a plugin
 # Note: fixtures with session scope need to be local
 pytest_plugins = ["dbt.tests.fixtures.project"]
+
 
 # This fixture is for customizing tests that need overrides in adapter
 # repos. Example in dbt.tests.adapter.basic.test_base.
@@ -26,7 +26,7 @@ def test_config():
             up_result = run_cmd(['docker-compose', '-f', compose_file, 'up', '-d'])
             if up_result[0]:
                 raise Exception(f'Failed to start docker: {up_result[2]}')
-            url = "http://{}:{}".format(os.environ.get('HOST_ENV_VAR_NAME'), 10723)
+            url = "http://{}:{}".format(os.environ.get('HOST_ENV_VAR_NAME', 'localhost'), 10723)
             wait_until_responsive(timeout=30.0, pause=0.5, check=lambda: is_responsive(url))
         except Exception as e:
             raise Exception('Failed to run docker-compose: {}', str(e))
@@ -37,6 +37,7 @@ def test_config():
             run_cmd(['docker-compose', '-f', compose_file, 'down', '-v'])
         except Exception as e:
             raise Exception('Failed to run docker-compose while cleaning up: {}', str(e))
+
 
 # The profile dictionary, used to write out profiles.yml
 # dbt will supply a unique schema per test, so we do not specify 'schema' here
