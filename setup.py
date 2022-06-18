338ce8802fb168c9db02a38a111c5d226f2b66c8
guykohen <guy@clickhouse.com>
2022-06-17 20:07:28 -0400

    Updated clickhouse-connect version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     },
     install_requires=[
         f'dbt-core~={dbt_version}',
-        'clickhouse-connect>=0.1.2',
+        'clickhouse-connect>=0.1.4',
     ],
     python_requires=">=3.7",
     platforms='any',
