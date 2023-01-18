dca56b8bdaa81a60c258b28ecacd0a6b159d090c
Geoff Genz <geoff@clickhouse.com>
2023-01-18 10:25:12 -0700

    Ensure that our unique session id isn't stomped on in custom settings

--- dbt/adapters/clickhouse/dbclient.py
+++ dbt/adapters/clickhouse/dbclient.py
@@ -59,6 +59,7 @@ class ChClientWrapper(ABC):
         self.database = credentials.schema
         custom_settings = credentials.custom_settings or {}
         self._conn_settings = custom_settings.copy()
+        self._conn_settings['session_id'] = f'dbt::{uuid.uuid4()}'
         if credentials.cluster_mode or credentials.database_engine == 'Replicated':
             self._conn_settings['database_replicated_enforce_synchronous_settings'] = '1'
             self._conn_settings['insert_quorum'] = 'auto'
