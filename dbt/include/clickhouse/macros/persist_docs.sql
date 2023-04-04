7afeec16986672cc5016dd4cb2bcd4291e47c3c9
Konstantin Ilchenko <k.ilchenko@appodeal.com>
2023-04-04 22:56:11 +0200

    Reduce number of MODIFY COMMENT queries count

--- dbt/include/clickhouse/macros/persist_docs.sql
+++ dbt/include/clickhouse/macros/persist_docs.sql
@@ -1,3 +1,7 @@
+{% macro one_alter_relation(relation, alter_comments) %}
+  alter table {{ relation }} {{ on_cluster_clause() }} {{ alter_comments }}
+{% endmacro %}
+
 {% macro one_alter_column_comment(relation, column_name, comment) %}
   alter table {{ relation }} {{ on_cluster_clause() }} comment column {{ column_name }} '{{ comment }}'
 {% endmacro %}
@@ -7,14 +11,22 @@
 {% endmacro %}
 
 {% macro clickhouse__persist_docs(relation, model, for_relation, for_columns) %}
+  {%- set alter_comments = [] %}
+
   {%- if for_relation and config.persist_relation_docs() and model.description -%}
-    {% do run_query(alter_relation_comment(relation, model.description)) %}
+    {% do alter_comments.append("modify comment '{comment}'".format(comment=model.description)) %}
   {%- endif -%}
 
   {%- if for_columns and config.persist_column_docs() and model.columns -%}
     {%- for column_name in model.columns -%}
       {%- set comment = model.columns[column_name]['description'] -%}
-      {% do run_query(one_alter_column_comment(relation, column_name, comment)) %}
+      {%- if comment %}
+        {% do alter_comments.append("comment column {column_name} '{comment}'".format(column_name=column_name, comment=comment)) %}
+      {%- endif %}
     {%- endfor -%}
+  {%- endif -%}
+
+  {%- if alter_comments | length > 0 -%}
+    {% do run_query(one_alter_relation(relation, alter_comments|join(', '))) %}
   {%- endif -%}
 {% endmacro %}
