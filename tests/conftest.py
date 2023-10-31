8a16ce23e44d4c305cac1aa6ce7bc8eb4cc676fd
Geoff Genz <geoff@clickhouse.com>
2023-10-31 10:22:57 -0600

    Initial 1.5.0 commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
+import os
+import time
+
+os.environ['TZ'] = 'UTC'
+time.tzset()
+
+
 # Import the standard integration fixtures as a plugin
 # Note: fixtures with session scope need to be local
 pytest_plugins = ["dbt.tests.fixtures.project"]
