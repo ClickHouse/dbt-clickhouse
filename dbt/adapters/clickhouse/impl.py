e4ab5bb2f4cb6cf3dfd04684a24b344835f12d2c
Cristhian Garcia <crisgarta8@gmail.com>
2024-04-01 13:43:30 -0500

    feat: add TTL support

--- dbt/adapters/clickhouse/impl.py
+++ dbt/adapters/clickhouse/impl.py
@@ -41,6 +41,7 @@ class ClickHouseConfig(AdapterConfig):
     order_by: Optional[Union[List[str], str]] = 'tuple()'
     partition_by: Optional[Union[List[str], str]] = None
     sharding_key: Optional[Union[List[str], str]] = 'rand()'
+    ttl: Optional[Union[List[str], str]] = None
 
 
 class ClickHouseAdapter(SQLAdapter):
