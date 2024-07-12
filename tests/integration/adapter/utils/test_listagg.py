edaf5ec9e8f2d3e7c299c7a0bec7d0e7163619aa
Igor Bolshakov <ibolsch@gmail.com>
2024-07-12 09:45:38 +0300

    import base

--- tests/integration/adapter/utils/test_listagg.py
+++ tests/integration/adapter/utils/test_listagg.py
@@ -1,13 +1,12 @@
 import pytest
 
-from dbt.tests.adapter.utils.base_utils import BaseUtils
+from dbt.tests.adapter.utils.test_listagg import BaseListagg
 from dbt.tests.adapter.utils.fixture_listagg import (
-    models__test_listagg_sql,
     models__test_listagg_yml,
     seeds__data_listagg_csv,
-    seeds__data_listagg_output_csv,
 )
 from dbt.tests.util import run_dbt
+
 
 models__test_listagg_custom_sql = """
 select
@@ -18,23 +17,6 @@ from {{ ref('data_listagg') }} group by group_col
 """
 
 
-class BaseListagg(BaseUtils):
-    @pytest.fixture(scope="class")
-    def seeds(self):
-        return {
-            "data_listagg.csv": seeds__data_listagg_csv,
-            "data_listagg_output.csv": seeds__data_listagg_output_csv,
-        }
-
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "test_listagg.yml": models__test_listagg_yml,
-            "test_listagg.sql": self.interpolate_macro_namespace(
-                models__test_listagg_sql, "listagg"
-            ),
-        }
-
 class TestListagg(BaseListagg):
     pass
 
@@ -44,7 +26,6 @@ class TestCustomListagg:
     def seeds(self):
         return {
             "data_listagg.csv": seeds__data_listagg_csv,
-            "data_listagg_output.csv": seeds__data_listagg_output_csv,
         }
 
     @pytest.fixture(scope="class")
