58a34b1a68012c3be37d16b021f3101df73e286d
Jose Muñoz <jose.munoz@clickhouse.com>
2025-09-17 17:15:03 +0200

    Fix test_types

--- tests/integration/adapter/unit_testing/test_types.py
+++ tests/integration/adapter/unit_testing/test_types.py
@@ -14,16 +14,18 @@ class TestingTypes(BaseUnitTestingTypes):
             [
                 "TIMESTAMP '2013-11-03 00:00:00'",
                 "2013-11-03 00:00:00",
-            ],  # overrided: no "-0" support
+            ],  # Overridden: no "-0" support
             [
                 "'2023-10-29 01:30:00'::DateTime('UTC')",
                 "2013-11-03 00:00:00",
-            ],  # overrided: no "-0" support and timezones expressed differently
+            ],  # Overridden: no "-0" support and timezones expressed differently
             ["'1'::numeric", "1"],
-            [
-                """'{"bar": "baz", "balance": 7.77, "active": false}'::json""",
-                """'{"bar": "baz", "balance": 7.77, "active": false}'""",
-            ],
+
+            # Not fully supported types:
+            # [
+            # """'{"bar": "baz", "balance": 7.77, "active": false}'::json""",
+            #     """'{"bar": "baz", "balance": 7.77, "active": false}'""",
+            # ],
             # TODO: support complex types <-- this TODO came from the DBT test.
             # ["ARRAY['a','b','c']", """'{"a", "b", "c"}'"""],
             # ["ARRAY[1,2,3]", """'{1, 2, 3}'"""],
