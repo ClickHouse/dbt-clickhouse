0006cd926341fa5cbec3cd835e8a03c6f1638ea7
MaelleBaillet5 <maelle.baillet@arkhn.com>
2025-10-22 17:32:18 +0200

    docs: add comment to explain clickhouse__safe_cast macro goal

--- dbt/include/clickhouse/macros/utils/safe_cast.sql
+++ dbt/include/clickhouse/macros/utils/safe_cast.sql
@@ -1,4 +1,7 @@
--- This macro allows you to set a default value for columns based on their type.
+-- This macro provides type-safe casting with automatic default values for ClickHouse types.
+-- When the literal string 'null' is passed as the field parameter, it returns the ClickHouse
+-- default value for the specified type. This is primarily used in unit test fixtures to avoid
+-- having to specify all non-nullable columns.
 
 {% macro clickhouse__safe_cast(field, dtype) %}
   {%- if field == 'null' -%}
