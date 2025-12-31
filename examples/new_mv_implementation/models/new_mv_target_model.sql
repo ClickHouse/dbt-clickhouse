e041924fabe4687324bfe35d2fafca42fc97c9db
Jose Muñoz <jose.munoz@clickhouse.com>
2025-12-31 18:57:41 +0100

    Don't recreate tables in `dbt run` if they are used by dbt mvs.

--- examples/new_mv_implementation/models/new_mv_target_model.sql
+++ examples/new_mv_implementation/models/new_mv_target_model.sql
@@ -3,8 +3,7 @@
     materialized='table',
     engine='SummingMergeTree()',
     order_by='(event_date, event_type)',
-    partition_by='toYYYYMM(event_date)',
-    on_schema_change='sync_all_columns'
+    partition_by='toYYYYMM(event_date)'
   )
 }}
 
@@ -13,7 +12,6 @@ SELECT
     toDate(now()) AS event_date,
     '' AS event_type,
     toUInt64(0) AS event_count,
-    toFloat64(0) AS total_value,
-    toUInt64(0) AS the_new_column
+    toFloat64(0) AS total_value
 WHERE 0  -- Creates empty table with correct schema
 
