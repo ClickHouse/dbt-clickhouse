a2959563a5d1affb5cbe9f70107c600c0b8a152f
igorvoltaic <ibolsch@gmail.com>
2026-05-27 09:20:30 +0300

    Apply suggestion from @igorvoltaic
    use `CLONE` to zero-copy data

--- dbt/include/clickhouse/macros/materializations/clone.sql
+++ dbt/include/clickhouse/macros/materializations/clone.sql
@@ -3,5 +3,5 @@
 {% endmacro %}
 
 {% macro clickhouse__create_or_replace_clone(this_relation, defer_relation) %}
-    create or replace table {{ this_relation.render() }} {{ on_cluster_clause(this_relation)}} as {{ defer_relation.render() }}
+    CREATE OR REPLACE TABLE {{ this_relation.render() }} {{ on_cluster_clause(this_relation)}} CLONE AS {{ defer_relation.render() }}
 {% endmacro %}
